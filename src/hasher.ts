0fc90d1105430714d480fd082086ede8b7d1ba6b
Dima Osipa <dima.osipa@gmail.com>
2023-05-18 13:15:07 -0700

    ignore symlinks

--- src/hasher.ts
+++ src/hasher.ts
@@ -21,15 +21,23 @@ export async function hashDirectory(directoryPath: string, algo: string = 'sha25
 
 async function getFilesRecursively(directoryPath: string, patterns: string[]): Promise<string[]> {
   const entries = await fs.promises.readdir(directoryPath, { withFileTypes: true });
+
   const filePaths = await Promise.all(
     entries.map(async (entry) => {
       const fullPath = path.join(directoryPath, entry.name);
-      return entry.isDirectory()
-        ? getFilesRecursively(fullPath, patterns)
-        : (matchesPatterns(fullPath, patterns) ? fullPath : []);
+      if (entry.isSymbolicLink()) {
+        return [];  // Ignore symlinks
+      } else if (entry.isDirectory()) {
+        return getFilesRecursively(fullPath, patterns);
+      } else if (matchesPatterns(fullPath, patterns)) {
+        return [fullPath];
+      } else {
+        return [];
+      }
     })
   );
 
+  // Flatten the array of paths
   return Array.prototype.concat(...filePaths);
 }
 
