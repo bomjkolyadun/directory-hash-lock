5cda704c5b87de25758cc6cf3f7e2c1b76e674ad
Dima Osipa <1094629+bomjkolyadun@users.noreply.github.com>
2023-05-30 19:05:00 -0700

    feat: frozen lockfile (#1)
    * initial frozen lock file support
    
    * updates readme

--- src/argsParser.ts
+++ src/argsParser.ts
@@ -1,4 +1,4 @@
-export function parseArgs(args: string[]): { directoryPath: string; patterns: string[]; lockFilePath: string } {
+export function parseArgs(args: string[]): { directoryPath: string; patterns: string[]; lockFilePath: string; frozen: boolean } {
   const argMap = args.reduce<{ [key: string]: string }>((acc, arg) => {
     const [key, value] = arg.split('=');
     acc[key] = value;
@@ -8,6 +8,7 @@ export function parseArgs(args: string[]): { directoryPath: string; patterns: st
   const directoryPath = argMap['--path'] || './patches';
   const patterns = argMap['--patterns'] ? argMap['--patterns'].split(',') : [];
   const lockFilePath = `${directoryPath}.lock`;
+  const frozen = args.includes('--frozen-lockfile');
 
-  return { directoryPath, patterns, lockFilePath };
+  return { directoryPath, patterns, lockFilePath, frozen };
 }
--- tests/argsParser.test.ts
+++ tests/argsParser.test.ts
@@ -2,20 +2,22 @@ import { parseArgs } from '../src/argsParser';
 
 describe('argsParser', () => {
     it('should parse command-line arguments correctly', () => {
-        const args = ['--path=test/path', '--patterns=*.js,*.ts'];
-        const { directoryPath, patterns, lockFilePath } = parseArgs(args);
+        const args = ['--path=test/path', '--patterns=*.js,*.ts', '--frozen-lockfile'];
+        const { directoryPath, patterns, lockFilePath, frozen } = parseArgs(args);
 
         expect(directoryPath).toBe('test/path');
         expect(patterns).toEqual(['*.js', '*.ts']);
         expect(lockFilePath).toBe('test/path.lock');
+        expect(frozen).toBe(true);
     });
 
     it('should provide default values if arguments are not given', () => {
         const args: string[] = [];
-        const { directoryPath, patterns, lockFilePath } = parseArgs(args);
+        const { directoryPath, patterns, lockFilePath, frozen } = parseArgs(args);
 
         expect(directoryPath).toBe('./patches');
         expect(patterns).toEqual([]);
         expect(lockFilePath).toBe('./patches.lock');
+        expect(frozen).toBe(false);
     });
 });
